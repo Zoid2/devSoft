4168cc479031a31893bed03771643b3b9eb8f3c3
Ziyad Hamed <zydmain@gmail.com>
2024-09-16 11:33:40 -0400

    k04

---
+++ 04_rando-dict/kewes.py
@@ -0,0 +1,25 @@
+'''
+Ziyad Hamed
+Team Sizzle
+SoftDev
+K04 :: Python dictionairies and random selection
+2024-09-16
+'''
+from random import randint
+krewes = {
+           4: [ 
+    'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+    'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+    'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+    'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+],
+           5: [ 
+                'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+                'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+                'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+                'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN' 
+              ]
+         }
+period = randint(4, 5)
+studentCount = krewes[period][randint(0, len(krewes[period]) - 1)]
+print(studentCount)
