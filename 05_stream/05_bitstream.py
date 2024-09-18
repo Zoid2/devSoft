311401005e5fdd882f2eaeba689cfec7322f66d0
Ziyad Hamed <zydmain@gmail.com>
2024-09-18 11:47:35 -0400

    work

--- 05_stream/05_bitstream.py
+++ 05_stream/05_bitstream.py
@@ -1,4 +1,12 @@
-file = open("krewes.txt", "r")
-print(file.read())
-data = file.read().split("@@@")
-print(data)
+import random
+f = open("krewes.txt")
+all_duckies = f.read()
+devos = {}
+ducky_group = all_duckies.split('@@@')
+for ducky in ducky_group:
+    if "$$$" in ducky:
+        ducky = ducky.split("$$$")
+        devos[ducky[1]] = list((ducky[0], ducky[2]))
+devo = random.choice(list(devos.keys()))
+
+print("Devo: " + devo + ", " + "Period: " + devos[devo][0] + ", Duck: " + devos[devo][1])
