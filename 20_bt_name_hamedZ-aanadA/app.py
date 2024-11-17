db69efe9e02e1e0c7c4a461b3ca50311a79383b4
Ziyad Hamed <zydmain@gmail.com>
2024-11-17 15:27:37 -0500

    grid demo

--- 20_bt_name_hamedZ-aanadA/app.py
+++ 20_bt_name_hamedZ-aanadA/app.py
@@ -1,12 +1,31 @@
 # Ziyad Hamed
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "123"
+
+columns = 10
+rows = 10
 
 @app.route("/")
 def index():
-    return render_template("index.html", num_cols=4, num_rows=3)
+    return render_template("index.html", num_cols=columns, num_rows=rows)
+
+@app.route("/update", methods=["post"])
+def update():
+    global columns, rows
+    print(request.method)
+    print(request)
+    print(request.args)
+    if request.method == "POST":
+        session["cols"] = request.form["columns"]
+        session["rows"] = request.form["rows"]
+        print("Session: ")
+        print(session)
+        columns = int(session["cols"])
+        rows = int(session["rows"])
+    return render_template("index.html", num_cols=columns, num_rows=rows)
 
 if __name__ == "__main__":  # false if this file imported as module
     # enable debugging, auto-restarting of server when this file is modified
